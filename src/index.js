376ee2a4ad1a338f2c07f60a8c387ee83ebdb5cd
alexandru.catalisan@devops.ncia.nato.int <alexandru.catalisan@devops.ncia.nato.int>
2019-11-13 15:33:26 +0100

    initializing store and etc

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import ReduxPromise from 'redux-promise';
 import './index.css';
-import App from './App';
+import Layout from './components/layout/layout';
 import * as serviceWorker from './serviceWorker';
+import reducers from './state/reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// creating a store for the redux structure
+const store = createStore(
+    reducers,
+    applyMiddleware(logger, ReduxPromise)
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+    <Provider store={store}>
+        <Layout />
+    </Provider>
+    , document.getElementById('root'));
 serviceWorker.unregister();
